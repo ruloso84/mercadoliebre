486436031b57360635ab09eeb77fe3306e1ed66c
rulso84 <javiermirandanieto@gmail.com>
2022-04-13 21:43:10 -0300

    Add

---
+++ app.js
@@ -0,0 +1,25 @@
+let express = require('express');
+
+let app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.listen(3030, () => console.log('Servidor corriendo en http://localhost:3030'));
+
+/* const publicPath = path.resolve(__dirname, './public'); */
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
